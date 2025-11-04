0c2332c92c319f5e8c69a397738b19394535838b
momo3dcourse <momo3dcourse@gmail.com>
2025-11-04 04:19:05 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,26 +1,28 @@
-{pkgs}: {
-  channel = "stable-24.05";
-  packages = [
-    pkgs.nodejs_20
-  ];
-  idx.extensions = [
-    
-  ];
+{ pkgs, ... }:
+
+{
+  # See https://developers.google.com/idx/guides/customize-idx-env
+  # for more details on customizing your environment.
+  channels.nixpkgs = "unstable"; # or "stable"
+  packages = [ pkgs.nodejs_20 ];
+
+  # `idx.previews` defines the previews that show up in the IDX sidebar.
   idx.previews = {
-    previews = {
-      web = {
-        command = [
-          "npm"
-          "run"
-          "dev"
-          "--"
-          "--port"
-          "$PORT"
-          "--hostname"
-          "0.0.0.0"
-        ];
-        manager = "web";
-      };
+    # The following `web` preview is the default, and is optional if you
+    # just want a single preview for your primary web service.
+    web = {
+      # The command to start your web server.
+      # The command must not exit.
+      command = [ "npm" "run" "dev" ];
+      # The port that your server will be listening on.
+      port = 9002;
+    };
+    # The following preview is for the Genkit developer UI.
+    genkit = {
+      # The command to start the Genkit developer UI.
+      command = [ "npm" "run" "genkit:watch" ];
+      # The port that the Genkit UI will be listening on.
+      port = 4000;
     };
   };
 }
